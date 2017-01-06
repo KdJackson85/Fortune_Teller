2a62f1034810077a1fb6317c555267ba73668a5c
Keith Jackson <kj5thguitar@gmail.com>
2017-01-06 17:55:58 -0500

    second try.

--- Project_1-Fortune Teller/Program.cs
+++ Project_1-Fortune Teller/Program.cs
@@ -11,8 +11,8 @@ namespace Project_1_Fortune_Teller
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, and welcome to Fortune Teller!");
-            Console.WriteLine("Fill out the questionaire and see what your future holds!");
-            
+            Console.WriteLine("Fill out the questionnaire and see what your future holds!");
+
             Console.WriteLine("What is your first name?");
             string firstName = (Console.ReadLine());
 
@@ -21,6 +21,15 @@ namespace Project_1_Fortune_Teller
 
             Console.WriteLine("How old are you?");
             int age = int.Parse(Console.ReadLine());
+            if (age % 2 == 0)
+            {
+                string retire = ("You will retire in 30 years. :[");
+            }
+            else
+            {
+                string retire = ("You will retire in 15 minutes. Bon Voyage!");
+            }
+
 
             Console.WriteLine("What month were you born? (\"MM\")");
             int birthMonth = int.Parse(Console.ReadLine());
@@ -31,7 +40,35 @@ namespace Project_1_Fortune_Teller
             Console.WriteLine("How many siblings do you have?");
             int sib = int.Parse(Console.ReadLine());
 
+            if (sib == 0)
+                {
+                string vacation = ("Maui");
+                }
+            if (sib == 1);
+                {
+                string vacation = ("Paris, France");
+                }
+            if (sib == 2)
+                {
+                string vacation = ("Youngstown,OH");
+                }
+            if (sib >= 3)
+                {
+                string vacation = ("Sidney, Australia");
+                }
+            else
+                {
+                string vacation = ("Your ex's parents backyard");
+                }
 
+
+            
+
+
+                         
+
+
+            
 
 
         }
