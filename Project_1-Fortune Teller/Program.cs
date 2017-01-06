1ec94c0b657b2f10c3000a18e2576725233bef32
Keith Jackson <kj5thguitar@gmail.com>
2017-01-06 16:52:39 -0500

    This is my first commit.

--- Project_1-Fortune Teller/Program.cs
+++ Project_1-Fortune Teller/Program.cs
@@ -10,6 +10,30 @@ namespace Project_1_Fortune_Teller
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello, and welcome to Fortune Teller!");
+            Console.WriteLine("Fill out the questionaire and see what your future holds!");
+            
+            Console.WriteLine("What is your first name?");
+            string firstName = (Console.ReadLine());
+
+            Console.WriteLine("What is your last name?");
+            string lastName = (Console.ReadLine());
+
+            Console.WriteLine("How old are you?");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("What month were you born? (\"MM\")");
+            int birthMonth = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Whats your favorite ROYGBIV color? \nNot sure what that means? \njust say \"Help.\"");
+            string favColor = (Console.ReadLine());
+
+            Console.WriteLine("How many siblings do you have?");
+            int sib = int.Parse(Console.ReadLine());
+
+
+
+
         }
     }
 }
